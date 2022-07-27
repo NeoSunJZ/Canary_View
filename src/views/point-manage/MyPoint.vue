94df492bb967ede7a99726abc1c78abd82d52954
cicada-language-ice <474743988@qq.com>
2022-07-27 10:32:44 +0800

    [构建中]修改节点卡片为节点列表；准备做增删查功能

--- src/views/point-manage/MyPoint.vue
+++ src/views/point-manage/MyPoint.vue
@@ -11,19 +11,24 @@
       <a-breadcrumb-item>我的节点</a-breadcrumb-item>
     </template>
     <template v-slot:content>
-      <ServerInfoCard></ServerInfoCard>
+      <ServerInfoTable :serverInfo="server1"></ServerInfoTable>
     </template>
   </MainPageNavigation>
 </template>
 
 <script>
 import MainPageNavigation from '@/components/MainPageNavigation.vue';
-import ServerInfoCard from '@/components/ServerInfoCard.vue';
+import ServerInfoTable from '@/components/ServerInfoTable.vue';
+import { ref } from 'vue';
 export default {
   name: 'MyPoint',
-  components: { MainPageNavigation, ServerInfoCard },
+  components: { MainPageNavigation, ServerInfoTable },
   setup() {
+    const server1 = ref({ name: 'Server1', ip: '10.0.0.55', port: '8888' });
     //
+    return {
+      server1,
+    };
   },
 };
 </script>
