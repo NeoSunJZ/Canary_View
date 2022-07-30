0e75598bea73d0fa62df8d1a2974d4463f18fe64
cicada-language-ice <474743988@qq.com>
2022-07-30 23:41:32 +0800

    [构建中]编写了增删改差前端函数成功联通；编辑可以单个元素；新建弹出窗口构建中

--- src/api/node-api/nodeinfo.js
+++ src/api/node-api/nodeinfo.js
@@ -23,3 +23,85 @@ export async function getNodeInfo(pageNum, pageSize = 2) {
     });
     return data;
 }
+
+/**
+ * @description: 用于请求增加一个节服务器点
+ * @param {String?} userToken 用户身份令牌，还没用上
+ * @param {String} ip 
+ * @param {String} port
+ * @param {String} name 服务器名
+ * @param {String} desc 服务器描述
+ * @return 返回是否成功增加
+ */
+export async function addNodeInfo(ip, port, name, desc) {
+    let success = false;
+    await axiosPlugin({
+        method: "post",
+        url: "v2/resource/NodeInfo/addNodeInfo",
+        params: {
+            ip: ip,
+            port: port,
+            nodeName: name,
+            nodeDesc: desc
+        },
+    }).then((response) => {
+        if (response.data.state === "SUCCESS") {
+            success = true
+        }
+    });
+    return success;
+}
+
+/**
+ * @description: 用于请求删除一个服务器节点
+ * @param {String?} userToken 用户身份令牌，还没用上
+ * @param {Number} nodeID 删除的节点ID
+ * @return 返回是否成功
+ */
+export async function deleteNodeInfo(nodeID) {
+    let success = false;
+    await axiosPlugin({
+        method: "get",
+        url: "v2/resource/NodeInfo/deleteNodeInfo",
+        params: {
+            nodeID: nodeID
+        },
+    }).then((response) => {
+        if (response.data.state === "SUCCESS") {
+            success = true
+        }
+    });
+    return success;
+}
+
+/**
+ * @description: 用于请求更新一个服务器节点
+ * @param {String?} userToken 用户身份令牌，还没用上
+ * @param {Number} nodeID
+ * @param {String} ip 
+ * @param {String} port
+ * @param {String} name 服务器名
+ * @param {String} desc 服务器描述
+ * @param {Number} nodeID 删除的节点ID
+ * @return 返回是否成功
+ */
+export async function updateNodeInfo(nodeID, ip, port, name, desc) {
+    let success = false;
+    await axiosPlugin({
+        method: "get",
+        url: "v2/resource/NodeInfo/updateNodeInfo",
+        params: {
+            nodeID: nodeID,
+            ip: ip,
+            port: port,
+            nodeName: name,
+            nodeDesc: desc
+
+        },
+    }).then((response) => {
+        if (response.data.state === "SUCCESS") {
+            success = true
+        }
+    });
+    return success;
+}
--- src/components/ServerInfoTable.vue
+++ src/components/ServerInfoTable.vue
@@ -50,40 +50,72 @@
 
   <a-button class="editable-add-btn" style="margin-bottom: 8px" @click="handleAdd">新增节点</a-button>
 
+  <a-modal v-model:visible="visible" okText="确定" cancelText="取消" width="1000px" @ok="handleOk" :closable='false'>
+    123
+  </a-modal>
+
   <a-table bordered :data-source="dataSource" :columns="columns">
     <template #bodyCell="{ column, text, record }">
+
       <template v-if="column.dataIndex === 'name'">
         <div class="editable-cell">
-          <div v-if="editableData[record.key]" class="editable-cell-input-wrapper">
+          <div v-if="editableData[record.key]&&editableElement[record.key]=='name'" class="editable-cell-input-wrapper">
             <a-input v-model:value="editableData[record.key].name" @pressEnter="save(record.key)" />
             <check-outlined class="editable-cell-icon-check" @click="save(record.key)" />
           </div>
           <div v-else class="editable-cell-text-wrapper">
             {{ text || ' ' }}
-            <edit-outlined class="editable-cell-icon" @click="edit(record.key)" />
+            <edit-outlined class="editable-cell-icon" @click="edit(record.key,'name')" />
           </div>
         </div>
       </template>
+
       <template v-else-if="column.dataIndex === 'ip'">
         <div class="editable-cell">
-          <div v-if="editableData[record.key]" class="editable-cell-input-wrapper">
+          <div v-if="editableData[record.key]&&editableElement[record.key]=='ip'" class="editable-cell-input-wrapper">
             <a-input v-model:value="editableData[record.key].ip" @pressEnter="save(record.key)" />
             <check-outlined class="editable-cell-icon-check" @click="save(record.key)" />
           </div>
           <div v-else class="editable-cell-text-wrapper">
             {{ text || ' ' }}
-            <edit-outlined class="editable-cell-icon" @click="edit(record.key)" />
+            <edit-outlined class="editable-cell-icon" @click="edit(record.key,'ip')" />
           </div>
         </div>
       </template>
+
+      <template v-else-if="column.dataIndex === 'port'">
+        <div class="editable-cell">
+          <div v-if="editableData[record.key]&&editableElement[record.key]=='port'" class="editable-cell-input-wrapper">
+            <a-input v-model:value="editableData[record.key].port" @pressEnter="save(record.key)" />
+            <check-outlined class="editable-cell-icon-check" @click="save(record.key)" />
+          </div>
+          <div v-else class="editable-cell-text-wrapper">
+            {{ text || ' ' }}
+            <edit-outlined class="editable-cell-icon" @click="edit(record.key,'port')" />
+          </div>
+        </div>
+      </template>
+
+      <template v-else-if="column.dataIndex === 'description'">
+        <div class="editable-cell">
+          <div v-if="editableData[record.key]&&editableElement[record.key]=='description'" class="editable-cell-input-wrapper">
+            <a-input v-model:value="editableData[record.key].description" @pressEnter="save(record.key)" />
+            <check-outlined class="editable-cell-icon-check" @click="save(record.key)" />
+          </div>
+          <div v-else class="editable-cell-text-wrapper">
+            {{ text || ' ' }}
+            <edit-outlined class="editable-cell-icon" @click="edit(record.key,'description')" />
+          </div>
+        </div>
+      </template>
+
       <template v-else-if="column.dataIndex === 'operation'">
-
-        <a @click="getStatus(record.ip,record.port)">查询状态</a>
-
+        <a @click="getStatus(record.ip,record.port)">刷新</a>
         <a-popconfirm v-if="dataSource.length" title="Sure to delete?" @confirm="onDelete(record.key)">
-          <a> 删除节点</a>
+          <a> 删除</a>
         </a-popconfirm>
       </template>
+
     </template>
   </a-table>
 </template>
@@ -94,7 +126,7 @@
 import { defineComponent, ref, computed, reactive, onBeforeMount } from 'vue';
 import { DeploymentUnitOutlined, ClusterOutlined, CheckOutlined, EditOutlined } from '@ant-design/icons-vue';
 import { cloneDeep, forEach } from 'lodash-es';
-import { getNodeInfo } from '@/api/node-api/nodeinfo';
+import { getNodeInfo, addNodeInfo, deleteNodeInfo, updateNodeInfo } from '@/api/node-api/nodeinfo';
 import { message } from 'ant-design-vue';
 
 export default defineComponent({
@@ -106,9 +138,11 @@ export default defineComponent({
     EditOutlined,
   },
   props: {
-    serverInfo: Object,
+    //
   },
   setup() {
+    const visible = ref(false);
+    // 表格显示的每一列的标题以及宽度
     const columns = [
       {
         title: '序号',
@@ -165,20 +199,18 @@ export default defineComponent({
         status: '已断开',
       },
     ]);
-    const print = (record) => {
-      console.log(editableData);
-      console.log(record);
-    };
     const count = computed(() => dataSource.value.length + 1);
     const editableData = reactive({});
-
-    const edit = (key) => {
+    const editableElement = ref({});
+    const edit = (key, element) => {
       editableData[key] = cloneDeep(dataSource.value.filter((item) => key === item.key)[0]);
+      editableElement.value[key] = element;
     };
 
     const save = (key) => {
       Object.assign(dataSource.value.filter((item) => key === item.key)[0], editableData[key]);
       delete editableData[key];
+      editableElement.value[key] = '';
     };
 
     const onDelete = (key) => {
@@ -188,30 +220,31 @@ export default defineComponent({
       console.log(ip, port);
     };
     const handleAdd = () => {
-      const newData = {
-        key: `${count.value}`,
-        name: `Edward King ${count.value}`,
-        ip: 32,
-        port: `London, Park Lane no. ${count.value}`,
-        description: `测试节点 ${count.value}`,
-      };
-      dataSource.value.push(newData);
+      visible.value = true;
+      // const newData = {
+      //   key: `${count.value}`,
+      //   name: `Edward King ${count.value}`,
+      //   ip: 32,
+      //   port: `London, Park Lane no. ${count.value}`,
+      //   description: `测试节点 ${count.value}`,
+      // };
+      // dataSource.value.push(newData);
     };
     onBeforeMount(async () => {
-      const data = await getNodeInfo(1, 2);
-
-      data.list.forEach((element) => {
-        console.log(element);
-      });
+      // const data = await getNodeInfo(1, 2);
+      // data.list.forEach((element) => {
+      //   console.log(element);
+      // });
     });
     return {
-      print,
+      visible,
       columns,
       onDelete,
       getStatus,
       handleAdd,
       dataSource,
       editableData,
+      editableElement,
       count,
       edit,
       save,
