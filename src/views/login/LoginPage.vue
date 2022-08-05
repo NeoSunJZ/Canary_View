6108ff27fc12f9e2b71eddf3284fe5d65ab47ed8
cicada-language-ice <474743988@qq.com>
2022-08-05 13:51:34 +0800

    格式小修

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,29 +1,21 @@
 <style scoped lang='less'>
 </style>
 <template>
-  <editor :height="300" :width="600" :initialValue="initialValue" @updateValue='changeContentValue' />
   <LoginBox></LoginBox>
 </template>
 
 <script>
-import { defineComponent, ref, computed } from 'vue';
+import { defineComponent, ref } from 'vue';
 import LoginBox from '@/components/LoginBox.vue';
-import tinyEditor from '@/components/TinyEditor.vue';
 
 export default defineComponent({
   name: 'LoginPage',
-  components: { LoginBox, editor: tinyEditor },
+  components: { LoginBox },
   props: {
     //
   },
   setup(props) {
-    const initialValue = ref('init');
-    const changeContentValue = (value) => {
-      console.log(value);
-    };
     return {
-      initialValue,
-      changeContentValue,
       print,
       //
     };
