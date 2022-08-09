a6dae9d67cd33cdb34e501b7c9842c2ced5c48a4
cicada-language-ice <474743988@qq.com>
2022-08-09 17:45:06 +0800

    修改StatusGrid组件中自己写的徽标为提供的组件

--- src/views/node-manage/components/StatusGrid.vue
+++ src/views/node-manage/components/StatusGrid.vue
@@ -1,58 +1,20 @@
 <style lang="less" scoped>
-.state {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  // justify-content: space-around;
-  &__greenpoint {
-    display: inline-block;
-    height: 5px;
-    width: 5px;
-    border-radius: 16px;
-    color: #fff;
-    background: rgb(119, 228, 119);
-    margin-right: 3px;
-  }
-  &__redpoint {
-    display: inline-block;
-    height: 5px;
-    width: 5px;
-    border-radius: 16px;
-    color: #fff;
-    background: rgb(175, 2, 2);
-    margin-right: 3px;
-  }
-  &__running {
-    margin-right: 10px;
-    color: rgb(8, 179, 8);
-  }
-  &__stopping {
-    margin-right: 10px;
-    color: rgb(237, 6, 6);
-  }
-}
 </style>
 
 
 <template>
+
   <div v-if="NodeStatus==='running'">
-    <div class="state">
-      <div class='state__greenpoint'></div>
-      <p class="state__running">运行中</p>
-      <a-button type="link" size="small" shape="circle">
-        <SyncOutlined @click="refresh" />
-      </a-button>
-    </div>
+    <a-badge status="success" text="运行中" />
+    <a-button type="link" size="small" shape="circle">
+      <SyncOutlined @click="refresh" />
+    </a-button>
   </div>
   <div v-else>
-    <div class="state">
-      <div class='state__redpoint'></div>
-      <p class="state__stopping">已断开</p>
-      <a-button type="link" size="small" shape="circle">
-        <SyncOutlined @click="refresh" />
-      </a-button>
-
-    </div>
+    <a-badge status="error" text="已断开" />
+    <a-button type="link" size="small" shape="circle">
+      <SyncOutlined @click="refresh" />
+    </a-button>
   </div>
 </template>
 
@@ -81,9 +43,9 @@ export default defineComponent({
       // 测试用，未完成
       let data = await getDeclarationOnly(props.ip, props.port);
       if (data == null) {
-        NodeStatus.value = 'running';
-      } else {
         NodeStatus.value = 'stopping';
+      } else {
+        NodeStatus.value = 'running';
       }
     };
     onBeforeMount(async () => {
