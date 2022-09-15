d4832fd8e1e58507502e8427ecdc6555f751e359
cicada-language-ice <474743988@qq.com>
2022-09-15 10:54:29 +0800

    [已完成]新增节点的校验规则

--- src/views/node-manage/components/AddNodeForm.vue
+++ src/views/node-manage/components/AddNodeForm.vue
@@ -2,41 +2,41 @@
  * @FileDescription: 新增节点表单，基本功能完成，逻辑是使用addNodeInfo提交，同时传值给父组件刷新
  * @Todo: 美化表单、校验规则
  -->
-
 <style lang="less" scoped>
+.add-node-button {
+  margin-bottom: 10px;
+}
 .title {
   margin-bottom: 15px;
 }
 </style>
 
 <template>
-
-  <a-button class="editable-add-btn" style="margin-bottom: 8px" @click="handleAdd">新增节点</a-button>
-
+  <a-button class="add-node-button" type="primary" @click="handleAdd">新增节点</a-button>
   <a-modal v-model:visible="visible" width="500px" :footer="null" :closable='false'>
     <h2 class='title'>新增节点</h2>
-    <a-form :model="formState" v-bind="layout" name="nest-messages" :validate-messages="validateMessages" @finish="onFinish">
-      <a-form-item :name="['node', 'name']" label="名称" :rules="[{ required: true }]">
-        <a-input v-model:value="formState.node.name" />
+    <a-form :model="formState" v-bind="layout" ref="formRef" name="new-node-info" :rules="rules" @finish="handleFinish" @finishFailed="handleFinishFailed">
+      <a-form-item has-feedback name="name" label="名称">
+        <a-input v-model:value="formState.name" />
       </a-form-item>
-      <a-form-item :name="['node', 'ip']" label="IP" :rules="[{ required: true }]">
-        <a-input v-model:value="formState.node.ip" />
+      <a-form-item has-feedback name="ip" label="IP">
+        <a-input v-model:value="formState.ip" />
       </a-form-item>
-      <a-form-item :name="['node', 'port']" label="端口" :rules="[{ required:true}]">
-        <a-input-number v-model:value="formState.node.port" />
+      <a-form-item has-feedback name="port" label="端口">
+        <a-input-number v-model:value="formState.port" />
       </a-form-item>
-      <a-form-item :name="['node', 'introduction']" label="简介">
-        <a-textarea v-model:value="formState.node.introduction" />
+      <a-form-item name="introduction" label="简介">
+        <a-textarea v-model:value="formState.introduction" style="height:100px" />
       </a-form-item>
       <a-form-item :wrapper-col="{ span: 14, offset: 3 }">
-        <a-button type="primary" html-type="submit" @click="handleOk">确定</a-button>
+        <a-button type="primary" html-type="submit">确定</a-button>
         <a-button style="margin-left: 10px" @click="resetForm">清空</a-button>
         <a-button style="margin-left: 10px" @click="visible=false">取消</a-button>
       </a-form-item>
     </a-form>
   </a-modal>
-
 </template>
+
 <script>
 import { defineComponent, reactive, ref } from 'vue';
 import { addNodeInfo } from '@/api/node-api/nodeinfo';
@@ -44,12 +44,89 @@ import { notification } from 'ant-design-vue';
 export default defineComponent({
   props: {},
   setup(props, context) {
-    const visible = ref(true);
+    const visible = ref(false);
 
-    // 确认添加节点
-    const handleOk = async () => {
-      let success = await addNodeInfo(formState.node.ip, formState.node.port, formState.node.name, formState.node.introduction);
-      // visible.value = false;
+    // 新增节点按钮
+    const handleAdd = () => {
+      visible.value = true;
+    };
+
+    const formRef = ref();
+    // 绑定的表单信息
+    const formState = reactive({
+      name: '',
+      ip: '',
+      port: '',
+      introduction: '',
+    });
+
+    // 表单布局
+    const layout = {
+      labelCol: {
+        span: 3,
+      },
+      wrapperCol: {
+        span: 19,
+      },
+    };
+
+    const isIP = (ip) => {
+      let reg = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;
+      return reg.test(ip);
+    };
+
+    const checkName = async (_rule, value) => {
+      if (!value) {
+        return Promise.reject('名称不能为空!');
+      }
+    };
+
+    const checkIP = async (_rule, value) => {
+      if (!value) {
+        return Promise.reject('IP不能为空!');
+      } else if (!isIP(value)) {
+        return Promise.reject('非法IP!');
+      }
+    };
+
+    const checkPort = async (_rule, value) => {
+      if (!value) {
+        return Promise.reject('端口不能为空!');
+      } else if (value > 65535 || value < 0) {
+        return Promise.reject('非法端口!');
+      }
+    };
+
+    const rules = {
+      name: [
+        {
+          required: true,
+          validator: checkName,
+          trigger: 'change',
+        },
+      ],
+      ip: [
+        {
+          required: true,
+          validator: checkIP,
+          trigger: 'change',
+        },
+      ],
+      port: [
+        {
+          required: true,
+          validator: checkPort,
+          trigger: 'change',
+        },
+      ],
+      introduction: [
+        //
+      ],
+    };
+    // 提交表单且数据验证成功
+    const handleFinish = async (values) => {
+      let success = await addNodeInfo(formState.ip, formState.port, formState.name, formState.introduction);
+      visible.value = false;
       if (success == false) {
         notification['error']({
           message: '添加失败',
@@ -66,69 +143,29 @@ export default defineComponent({
       context.emit('nodeInfo', formState);
     };
 
-    // 新增节点按钮
-    const handleAdd = () => {
-      visible.value = true;
-    };
-
     const resetForm = () => {
-      formState.node = {
-        name: '',
-        ip: '',
-        port: '',
-        introduction: '',
-      };
+      formRef.value.resetFields();
     };
 
-    // 表单布局
-    const layout = {
-      labelCol: {
-        xs: {
-          span: 24,
-        },
-        sm: {
-          span: 3,
-        },
-      },
-      wrapperCol: {
-        xs: {
-          span: 24,
-        },
-        sm: {
-          span: 20,
-        },
-      },
+    // 提交表单且数据验证失败
+    const handleFinishFailed = (errors) => {
+      console.log(errors);
     };
-
-    // 校验规则：未完成
-    const validateMessages = {
-      required: '${label}不能为空',
-      types: {
-        number: '${label} is not a valid number!',
-      },
-      number: {
-        range: '${label}必须是一个数字',
-      },
-    };
-
-    // 绑定的表单信息
-    const formState = reactive({
-      node: {
-        name: '',
-        ip: '',
-        port: '',
-        introduction: '',
-      },
-    });
 
     return {
+      formRef,
       visible,
-      handleOk,
+      rules,
+      checkName,
+      checkIP,
+      checkPort,
+      handleFinish,
+      handleFinishFailed,
       handleAdd,
       resetForm,
+      isIP,
       formState,
       layout,
-      validateMessages,
     };
   },
 });
