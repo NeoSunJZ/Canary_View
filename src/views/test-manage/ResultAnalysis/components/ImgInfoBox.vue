6b5d73d157586b79288e767e3ec2c95db6ad0748
flame737 <1608764663@qq.com>
2023-09-27 13:01:44 +0800

    完善结果分析图片信息的展示

--- src/views/test-manage/ResultAnalysis/components/ImgInfoBox.vue
+++ src/views/test-manage/ResultAnalysis/components/ImgInfoBox.vue
@@ -6,22 +6,23 @@
 
 <template>
   <a-spin tip="正在加载" :spinning="loading">
-    <a-card size="small" v-if="inferenceResultDataset[imgID]!=null" :title="'图片 '+imgID+' 在 '+modelName+' 上的预测结果'">
+    <a-card size="small" v-if="inferenceResultDataset[imgID] != null"
+      :title="'图片 ' + imgID + ' 在 ' + modelName + ' 上的预测结果'">
       <a-descriptions :column="4" :bordered="true" size="small">
-        <a-descriptions-item label="图片ID">{{imgID}}</a-descriptions-item>
-        <a-descriptions-item label="原始标签">{{inferenceResultDataset[imgID].ori}}</a-descriptions-item>
-        <a-descriptions-item label="干净预测标签">{{inferenceResultDataset[imgID].cleanInference}}</a-descriptions-item>
+        <a-descriptions-item label="图片ID">{{ imgID }}</a-descriptions-item>
+        <a-descriptions-item label="原始标签">{{ inferenceResultDataset[imgID].ori }}</a-descriptions-item>
+        <a-descriptions-item label="干净预测标签">{{ inferenceResultDataset[imgID].cleanInference }}</a-descriptions-item>
         <a-descriptions-item label="有效性">
           <a-badge status="success" text="有效样本" v-if="inferenceResultDataset[imgID].isEffective" />
           <a-badge status="error" text="无效样本(预测错误)" v-else />
         </a-descriptions-item>
       </a-descriptions>
       <a-tabs v-model:activeKey="advImgInfoKey" @change="loadAdvInfo">
-        <a-tab-pane v-for="(data,key) in inferenceResultDataset[imgID]['attack']" :key="key" :tab="key">
+        <a-tab-pane v-for="(data, key) in inferenceResultDataset[imgID]['attack']" :key="key" :tab="key">
           <a-descriptions :column="5" bordered size="small">
-            <a-descriptions-item label="对抗预测标签">{{data.inferenceImgLabel}}</a-descriptions-item>
+            <a-descriptions-item label="对抗预测标签">{{ data.inferenceImgLabel }}</a-descriptions-item>
             <a-descriptions-item label="迁移攻击">
-              {{data.baseModel != modelName ? "是" : "否"}}
+              {{ data.baseModel != modelName ? "是" : "否" }}
             </a-descriptions-item>
             <a-descriptions-item label="攻击状态">
               <a-badge status="success" text="攻击成功" v-if="data.isEffectiveAttackOnTrueLabel" />
@@ -32,22 +33,23 @@
               <a-badge status="error" text="偏转结果失败" v-else />
             </a-descriptions-item>
           </a-descriptions>
-          <a-descriptions :column="3" bordered size="small" v-if="advInfoList[data.advImgID]!=null">
-            <a-descriptions-item label="生成耗时">{{parseFloat(advInfoList[data.advImgID]['cost_time']).toFixed(5)}} 秒(s)</a-descriptions-item>
+          <a-descriptions :column="3" bordered size="small" v-if="advInfoList[data.advImgID] != null">
+            <a-descriptions-item label="生成耗时">{{ parseFloat(advInfoList[data.advImgID]['cost_time']).toFixed(5) }}
+              秒(s)</a-descriptions-item>
             <a-descriptions-item label="最大像素扰动">
-              {{parseFloat(advInfoList[data.advImgID]['adv_img_maximum_disturbance']).toFixed(7)}}
+              {{ parseFloat(advInfoList[data.advImgID]['adv_da_info']['maximum_disturbance']).toFixed(7) }}
             </a-descriptions-item>
             <a-descriptions-item label="像素平均欧式距离">
-              {{parseFloat(advInfoList[data.advImgID]['adv_img_euclidean_distortion']).toFixed(10)}}
+              {{ parseFloat(advInfoList[data.advImgID]['adv_da_info']['euclidean_distortion']).toFixed(10) }}
             </a-descriptions-item>
             <a-descriptions-item label="像素变化率">
-              {{parseFloat(advInfoList[data.advImgID]['adv_img_pixel_change_ratio']).toFixed(5)*100 +"%"}}
+              {{ parseFloat(advInfoList[data.advImgID]['adv_da_info']['pixel_change_ratio']).toFixed(5) * 100 + "%" }}
             </a-descriptions-item>
             <a-descriptions-item label="深度特征相似性">
-              {{parseFloat(advInfoList[data.advImgID]['adv_img_deep_metrics_similarity']).toFixed(7)}}
+              {{ parseFloat(advInfoList[data.advImgID]['adv_da_info']['deep_metrics_similarity']).toFixed(7) }}
             </a-descriptions-item>
             <a-descriptions-item label="结构特征相似性">
-              {{parseFloat(advInfoList[data.advImgID]['adv_img_low_level_metrics_similarity']).toFixed(7)}}
+              {{ parseFloat(advInfoList[data.advImgID]['adv_da_info']['low_level_metrics_similarity']).toFixed(7) }}
             </a-descriptions-item>
           </a-descriptions>
           <a-row :gutter="10">
@@ -58,7 +60,8 @@
             </a-col>
             <a-col :span="8">
               <div style="width:100%;padding:10px">对抗样本对比</div>
-              <a-image v-if="advInfoList[data.advImgID]!=null" :src="advInfoList[data.advImgID]['adv_img']['diff']" style="width:100%;padding:10px" />
+              <a-image v-if="advInfoList[data.advImgID] != null" :src="advInfoList[data.advImgID]['adv_img']['diff']"
+                style="width:100%;padding:10px" />
             </a-col>
           </a-row>
         </a-tab-pane>
